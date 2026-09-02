de66087ed06bc42ee5d5130093ae5288ad773f34
Jblessd <duprejesse3@gmail.com>
2026-09-02 00:08:01 -0400

    Change theme color meta tag value

--- netlify/edge-functions/pages.ts
+++ netlify/edge-functions/pages.ts
@@ -375,7 +375,7 @@ function page(opts: {
     `<meta charset="UTF-8"/>` +
     `<meta name="viewport" content="width=device-width, initial-scale=1.0"/>` +
     `<meta http-equiv="content-language" content="en-US"/>` +
-    `<meta name="theme-color" content="#080000"/>` +
+    `<meta name="theme-color" content="#0A0E16"/>` +
     `<title>${esc(opts.title)}</title>` +
     `<meta name="description" content="${esc(opts.description)}"/>` +
     `<link rel="canonical" href="${esc(opts.canonical)}"/>` +
